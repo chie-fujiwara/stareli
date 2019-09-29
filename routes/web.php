10b334e641943c4c0510961dcd355ee40eff047a
Naoto Shimoda <n104075n@gmail.com>
2019-09-29 13:56:05 +0900

    一部修正

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
