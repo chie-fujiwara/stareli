cfe8377d7c43e3cd614d2beab12bc46cd4cea394
chie <mimosa.chie@gmail.com>
2019-09-29 17:10:40 +0900

    welcome.blade.phpに追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,8 +67,47 @@
 </head>
 
 <body>
-    <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
+    <div class="container">
+        <div class="logo">
+            <img class="logo1" src="img/stareli_logo.png" alt="">
+            @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                <a href="{{ url('/home') }}">Home</a>
+                @else
+                <a href="{{ route('login') }}">Login</a>
+
+                @if (Route::has('register'))
+                <a href="{{ route('register') }}">Register</a>
+                @endif
+                @endauth
+            </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Sta/reli
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+
+
+    <!-- <div class="flex-center position-ref full-height"> -->
+    <!-- @if (Route::has('login'))
         <div class="top-right links">
             @auth
             <a href="{{ url('/home') }}">Home</a>
@@ -80,24 +119,48 @@
             @endif
             @endauth
         </div>
-        @endif
+        @endif -->
 
-        <div class="content">
-            <div class="title m-b-md">
-                Sta/reli
-            </div>
+    <!-- <div class="content">
+        <div class="title m-b-md">
+            Sta/reli
+        </div>
 
-            <div class="links">
-                <a href="https://laravel.com/docs">Docs</a>
-                <a href="https://laracasts.com">Laracasts</a>
-                <a href="https://laravel-news.com">News</a>
-                <a href="https://blog.laravel.com">Blog</a>
-                <a href="https://nova.laravel.com">Nova</a>
-                <a href="https://forge.laravel.com">Forge</a>
-                <a href="https://github.com/laravel/laravel">GitHub</a>
-            </div>
+        <div class="links">
+            <a href="https://laravel.com/docs">Docs</a>
+            <a href="https://laracasts.com">Laracasts</a>
+            <a href="https://laravel-news.com">News</a>
+            <a href="https://blog.laravel.com">Blog</a>
+            <a href="https://nova.laravel.com">Nova</a>
+            <a href="https://forge.laravel.com">Forge</a>
+            <a href="https://github.com/laravel/laravel">GitHub</a>
+        </div>
+    </div> -->
+    <!-- </div> -->
+</body>
+
+
+<body>
+    <div class="container">
+        <div class="logo">
+            <img class="logo1" src="img/stareli_logo.png" alt="">
         </div>
     </div>
+
+    <div class="container">
+        <div class="btn-wrapper">
+            <a href="" class="btn new">新規アカウント登録</a>
+            <p>または</p>
+            <a href="" class="btn login">ログイン</a>
+            <p class="line"></p>
+            <a href="" class="btn shop">登録店舗を探す</a>
+        </div>
+    </div>
+
+
+
 </body>
+
+
 
 </html>
